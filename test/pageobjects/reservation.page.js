e26c5b0787005a7f663125deef2b927001dfc477
Jonathan Gomez <jonathangomez3189@gmail.com>
2020-08-19 22:44:51 -0600

    Update Case 1

--- test/pageobjects/reservation.page.js
+++ test/pageobjects/reservation.page.js
@@ -2,37 +2,81 @@ const Page = require("./page")
 
 
 class reservationPage extends Page {
+    //Button Flights 
+    get btnFlights () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/nav/ul/li[2]/a')}
+    
+    //Navigation through Fly From 
+    get sltFlyFrom () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[1]/div/div[1]/div/div[2]/div/a')} 
+    get inputFlyFrom () {return $('//html/body/div[6]/div/input')}
+    get sltDpFrlyFrom () {return $('//html/body/div[6]/ul/li[1]/div')}
 
-    get inputFlights () {return $('//*[@id="search"]/div/div/div/div/div/nav/ul/li[2]/a')}
-    get inputFlyFrom () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[1]/div/div[1]/div/div[2]/div')} 
-    get inputFlyTo () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[1]/div/div[2]/div/div[2]/div')} 
-    get inputDepart () {return $('//*[@id="FlightsDateStart"]')}
-    get btnNextMonth () {return $('//*[@id="datepickers-container"]/div[9]/nav/div[3]')}
-    get btnDay()  {return $('.datepicker--cell datepicker--cell-day').$('div=16')}; 
+    //Navigation through Fly To 
+    get sltFlyTo () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[1]/div/div[2]/div/div[2]/div')}
+    get inputFlyTo () {return $('//html/body/div[7]/div/input')} 
+    get sltDpFlyTo () {return $('//html/body/div[7]/ul/li/div')} 
+
+    //Navigation Depart
+    get sltDepart () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/input')}
+    get btnNextMonth () {return $('//html/body/div[3]/div[9]/nav/div[3]')}
+    get btnDay()  {return $('//html/body/div[3]/div[9]/div/div/div[2]/div[25]')}; 
+    
+    //Members Selection
     get btnAdults () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[3]/div/div/div[1]/div/div[2]/div/span/button[1]')}
-    get btnChilds () {return $('/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[3]/div/div/div[2]/div/div[2]/div/span/button[1]')}
-    get btnInfants () {return $('/html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[3]/div/div/div[3]/div/div[2]/div/span/button[1]')}
+    get btnChilds () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[3]/div/div/div[2]/div/div[2]/div/span/button[1]')}
+    get btnInfants () {return $('//html/body/div[2]/div[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div/div/form/div/div/div[3]/div[3]/div/div/div[3]/div/div[2]/div/span/button[1]')}
+   
     get btnSearchFly () {return $('//*[@id="flights"]/div/div/form/div/div/div[3]/div[4]/button')}
 
-    //get btnCalendarDay () {return $('//*[@id="FlightsDateStart"]')}
+    //Button Flights 
+    getbtnFlights () {return this.btnFlights}
 
-    getinputFlights () {return this.inputFlights}
+    //Navigation through Fly From 
+    getsltFlyFrom () {return this.sltFlyFrom}
     getinputFlyFrom () {return this.inputFlyFrom}
+    getsltDpFrlyFrom () {return this.sltDpFrlyFrom}
+
+    //Navigation through Fly To
+    getsltFlyTo () {return this.sltFlyTo}
     getinputFlyTo () {return this.inputFlyTo}
-    getinputDepart () {return this.inputDepart}
+    getsltDpFlyTo () {return this.sltDpFlyTo}
+
+    //Navigation Depart
+    getsltDepart () {return this.sltDepart}    
     getbtnNextMonth () {return this.btnNextMonth}
     getbtnDay () {return this.btnDay}
+    
+    //Members Selection
     getbtnAdults () {return this.btnAdults}
     getbtnChilds () {return this.btnChilds}
     getbtnInfants () {return this.btnInfants}
+
     getbtnSearchFly () {return this.btnSearchFly}
 
     open () {
         return super.open('');
+    
     }
+
+    addFlyFrom(flyFromValue) {
+        this.sltFlyFrom.click();
+        this.inputFlyFrom.setValue(flyFromValue);
+        this.sltDpFrlyFrom.click(); 
+        browser.pause(2000);
+
+    }
+
+    addFlyTo(flyToValue) {
+        this.sltFlyTo.click();
+        this.inputFlyTo.setValue(flyToValue);
+        this.sltDpFlyTo.click(); 
+        browser.pause(2000);
+    }
+
+        //reservationPage.getinputFlyFrom().setValue('SJO');
+      //  reservationPage.getsltDpFrlyFrom().click();
+    
 
 }
 
-
-
 module.exports = new reservationPage();
+
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,11 +2,37 @@ const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 const reservationPage = require('../pageobjects/reservation.page');
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Reservation Page Details', () => {
+    it('Should search a Flight ', () => {
         reservationPage.open();
-
         browser.setTimeout({ 'pageLoad': 10000 });
+
+        reservationPage.getbtnFlights().click();
+        browser.pause(5000);
+        
+        reservationPage.addFlyFrom('SJO');
+        reservationPage.addFlyTo('TXL');
+
+        reservationPage.getsltDepart().click();
+        reservationPage.getbtnNextMonth().click();
+        reservationPage.getbtnDay().click();
+        browser.pause(2000);
+
+        for (var i = 0; i < 2; i++) {
+            reservationPage.getbtnAdults().click();
+         }
+         browser.pause(2000);
+
+        for (var y = 0; y < 2; y++) {
+            reservationPage.getbtnChilds().click();
+         }
+         browser.pause(2000);
+
+         reservationPage.getbtnInfants().click();
+         browser.pause(5000);
+
+         reservationPage.getbtnSearchFly().click();
+         browser.setTimeout({ 'pageLoad': 10000 });
 
     });
 });
